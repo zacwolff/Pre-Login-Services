32f9846e16ad9b12884fa3c329361001de59a066
Zac Wolff <zacharyhussein@gmail.com>
2015-03-04 09:47:35 +0000

    Prelogin Services

---
+++ Pre-Login Services/PrivacyPolicyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PrivacyPolicyTableViewController.h
+//  Pre-Login Services
+//
+//  Created by Zac Wolff on 3/4/15.
+//  Copyright (c) 2015 Zac Wolff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrivacyPolicyTableViewController : UITableViewController
+
+@end
