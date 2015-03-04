e8eebfa6bf54e4243326d95ea129ca1ee4483c58
Zac Wolff <zacharyhussein@gmail.com>
2015-03-04 07:44:36 +0000

    Initial Commit

---
+++ Pre-Login Services/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pre-Login Services
+//
+//  Created by Zac Wolff on 3/4/15.
+//  Copyright (c) 2015 Zac Wolff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pre-Login ServicesTests/Pre_Login_ServicesTests.m
@@ -0,0 +1,40 @@
+//
+//  Pre_Login_ServicesTests.m
+//  Pre-Login ServicesTests
+//
+//  Created by Zac Wolff on 3/4/15.
+//  Copyright (c) 2015 Zac Wolff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Pre_Login_ServicesTests : XCTestCase
+
+@end
+
+@implementation Pre_Login_ServicesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
